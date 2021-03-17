497c0310d5c41b766982dc5587a35fb24f584a23
shameera17 <shameera.carrim17@gmail.com>
2021-03-17 20:32:17 +0530

    add changes

--- src/containers_webPages/User_Seller/CartPage.js
+++ src/containers_webPages/User_Seller/CartPage.js
@@ -1,12 +1,68 @@
 import React, {useState} from "react";
-
+import {
+    Card,
+    CardContent,
+    CardMedia,
+    Button,
+    Typography,
+    Grid
+} from "@material-ui/core"
+import {makeStyles} from "@material-ui/core/styles"
+import {COLOURS} from "../../theme/colors"
+const useStyle = makeStyles(()=>({
+    block :{
+        marginTop: '20px'
+    },
+    row:{
+        display:'flex',
+        width: '600px',
+        margin: '5px',
+        
+    },
+    row1:{
+        width:'250px'
+    },
+    row2:{
+        width:'200px'
+    },
+    row3:{
+        width:'100px'
+    },
+    btn:{
+        backgroundColor : COLOURS.btnColor,
+        
+    }
+}));
 const CartPage = (props)=>{
+    const classes = useStyle();
     return(
         <div>
-            <h1>
-                Im in CartPage
-            </h1>
-            <h2>Hey</h2>
+          <Grid
+          container 
+          direction="column"
+          justify="center"
+          alignItems="center" 
+          spacing={1}> 
+             <div className={classes.block}>  
+              <Card className={classes.row} >
+               <CardContent className={classes.row1}>
+                   {/* image goes here */}
+                   <Typography>Product Name</Typography>
+                   <Typography>Product Price</Typography>
+               </CardContent>
+               <CardContent className={classes.row2}>
+                   <Typography>Quantity</Typography>
+                   <Typography>Price * Qty</Typography>
+                   <Typography>Delivery Method</Typography>
+                   <Typography>Availability</Typography>
+               </CardContent>
+               <CardContent className={classes.row3}>
+                   <Typography>Total Price</Typography>
+                   <Button className={classes.btn}>Check Out</Button>
+               </CardContent>
+            </Card> 
+            </div> 
+           </Grid>
         </div>
     )
 }
